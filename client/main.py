f4b0ef088994431504f6de07c8d3faaee76bdd4e
VladimirSIn <vldimir-sinitsa@ukr.net>
2018-08-06 14:34:01 +0300

    added basic UI

--- client/main.py
+++ client/main.py
@@ -1,9 +1,49 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import pyqtSlot
 
 
+class App(QWidget):
 
-def main():
-    pass
+    def __init__(self):
+        super().__init__()
+        self.title = "Let's talk"
+        self.left = 10
+        self.top = 10
+        self.width = 550
+        self.height = 550
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        #Send button
+        self.button = QPushButton('SEND', self)
+        self.button.resize(75, 75)
+        self.button.move(475, 480)
+        self.button.clicked.connect(self.on_click)
+
+        #Text input
+        self.textInput = QLineEdit(self)
+        self.textInput.resize(470, 60)
+        self.textInput.move(5, 485)
+
+        # Text viewer
+        self.textView = QLineEdit(self)
+        self.textView.resize(540, 470)
+        self.textView.move(5, 5)
+        self.textView.setReadOnly(True)
+
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        #TODO: implement "SEND" button functionality
+        pass
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
