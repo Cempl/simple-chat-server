90abc4b63fdcf20d1d448175cfd989ba4357105a
Viktor <viktor.markvardt@gmail.com>
2018-08-07 22:05:06 -0700

    added small example

--- server/main.py
+++ server/main.py
@@ -1,8 +1,31 @@
+from concurrent import futures
 
+import grpc
+import src.authentification_pb2
+import src.authentification_pb2_grpc
+
+
+class Auth(src.authentification_pb2_grpc.AuthServiceServicer):
+
+    def SignIn(self, request, context):
+
+        res = False
+
+        if request == "test" :
+            res = True
+
+        return src.authentification_pb2.Response(res)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    src.authentification_pb2_grpc.add_AuthServiceServicer_to_server(Auth(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
 
 
 def main():
-    pass
+    serve()
 
 
 if __name__ == "__main__":
