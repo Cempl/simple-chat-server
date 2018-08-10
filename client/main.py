b033e2ed48fb5ee543c7ab88366a1034f03cc1e1
VladimirSIn <vldimir-sinitsa@ukr.net>
2018-08-10 23:29:42 +0300

    just commit

--- client/main.py
+++ client/main.py
@@ -7,6 +7,9 @@ class App(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.button = QPushButton('SEND', self)
+        self.textInput = QLineEdit(self)
+        self.textView = QLineEdit(self)
         self.title = "Let's talk"
         self.left = 10
         self.top = 10
@@ -19,18 +22,15 @@ class App(QWidget):
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         #Send button
-        self.button = QPushButton('SEND', self)
         self.button.resize(75, 75)
         self.button.move(475, 480)
         self.button.clicked.connect(self.on_click)
 
         #Text input
-        self.textInput = QLineEdit(self)
         self.textInput.resize(470, 60)
         self.textInput.move(5, 485)
 
         # Text viewer
-        self.textView = QLineEdit(self)
         self.textView.resize(540, 470)
         self.textView.move(5, 5)
         self.textView.setReadOnly(True)
