3835d7062120502acdf30966ccaae32ca02444a4
Viktor <viktor.markvardt@gmail.com>
2018-08-13 09:51:40 -0700

    added .proto file, ignore folders, minor changes

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
+import time
 from concurrent import futures
 
 import grpc
 import src.authentification_pb2
 import src.authentification_pb2_grpc
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
 class Auth(src.authentification_pb2_grpc.AuthServiceServicer):
@@ -23,6 +27,12 @@ def serve():
     server.add_insecure_port('[::]:50051')
     server.start()
 
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
 
 def main():
     serve()
@@ -30,3 +40,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
